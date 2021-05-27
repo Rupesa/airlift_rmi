f690f2eaad29bc4021c13b871191ab624ff14f8f
Rita <rita.amante@ua.pt>
2021-05-27 10:17:22 +0100

    created project

---
+++ AirLift_89293_89264_ass3/src/airlift_89293_89264_ass3/AirLift_89293_89264_ass3.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package airlift_89293_89264_ass3;
+
+/**
+ *
+ * @author rita
+ */
+public class AirLift_89293_89264_ass3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
